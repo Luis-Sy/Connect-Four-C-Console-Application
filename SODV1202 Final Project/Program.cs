a12cef73fdbaaceaaafb3315c817ff2acc28eb32
Luis-Sy <74744192+Luis-Sy@users.noreply.github.com>
2023-06-23 22:48:19 -0600

    remade base code structure

--- SODV1202 Final Project/Program.cs
+++ SODV1202 Final Project/Program.cs
@@ -2,44 +2,109 @@
 {
     internal class Program
     {
-        public class ConnectFour
+        // create separate classes for game logic, game board, player
+        public static class ConnectFour
         {
             // properties for player objects
-            public string Name;
-            private static List<ConnectFour> PlayerList;
-            private static string[,] GameBoard;
-            public int Count;
+            private static List<Player> _PlayerList;
+            private static Connect4GameBoard _GameBoard;
 
-            public ConnectFour() // default player constructor
+            public static void NewGame(List<Player> players) // game initialization
             {
-                Name = "Anonymous";
+                _GameBoard = new Connect4GameBoard();
+                _PlayerList.Clear();
+                _PlayerList = players;
             }
+        }
+        public class Connect4GameBoard // gameboard class
+        {
+            public string[,] Board;
 
-            public static void AddPlayer(string name) // player creation function
+            public Connect4GameBoard() // initialize gameboard
             {
-                // create new player instance and add them to the list
-                ConnectFour player = new ConnectFour {Name = name };
-                PlayerList.Add(player);
-            }
-
-            private static void InitializeGameBoard() // create the gameboard
-            {
-                GameBoard = new string[6,7];
-                for(int i = 0; i < GameBoard.GetLength(0); i++)
+                Board = new string[6, 7];
+                for (int i = 0; i < Board.GetLength(0); i++)
                 {
-                    for(int j = 0; j < GameBoard.GetLength(1); j++)
+                    for (int j = 0; j < Board.GetLength(1); j++)
                     {
-                        GameBoard[i, j] = "-";
+                        Board[i, j] = "#";
                     }
                 }
             }
 
-
-
+            public bool CheckForWin(Player player, Token token)
+            {
+                return false;
+            }
         }
+        public abstract class Player // base player class
+        {
+            private Token _token;
+            public abstract bool PlaceToken(int col, int row);
+        }
+
+        // player subclasses
+        public class HumanPlayer : Player
+        {
+            private string _Name;
+            public HumanPlayer(string name)
+            {
+                _Name = name;
+            }
+            public override bool PlaceToken(int col, int row)
+            {
+                //todo
+                return false;
+            }
+        }
+
+        public class AIPlayer : Player
+        {
+            private string _Name;
+            public AIPlayer(string name)
+            {
+                _Name = name;
+            }
+            public override bool PlaceToken(int col, int row)
+            {
+                return false;
+            }
+        }
+
+        public class Token // token used by players
+        {
+            private string _piece;
+            public Token(string piece)
+            {
+                // ensure entered character is only a single value
+                _piece = piece.Substring(0);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+            ToDo:
+            -main function game logic
+                -setup
+                -check for win conditions
+                -check for tie condition
+            -classes
+                -player class
+                    -human and ai derived classes
+                -token class
+                    -used by players on the gameboard
+                -gameboard logic
+                    -placing pieces
+                    -check for invalid placements
+                    -check for victory conditions
+                
+            Bonus:
+            -configurable game options
+                -more than 2 players
+                -custom board size
+                -custom player tokens
+             */
         }
     }
 }
